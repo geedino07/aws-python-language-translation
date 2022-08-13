27d45937cd7cbc592059e02b35f3bad60e62c8b3
Gerald.okeke <okekegerald@gmail.com>
2022-08-13 01:49:26 +0100

    added cli

--- src/translate.py
+++ src/translate.py
@@ -1,8 +1,8 @@
 import boto3
-import click
+#import click
 
 client = boto3.client('translate', region_name="us-east-1")
 text = "estoy enamorado de mi esposa"
 result = client.translate_text(Text=text, SourceLanguageCode="auto", TargetLanguageCode="en")
-print (result)
-#print(result['TranslatedText']) to filter and print only translated text gerald
+#print (result)
+print(result['TranslatedText']) 
